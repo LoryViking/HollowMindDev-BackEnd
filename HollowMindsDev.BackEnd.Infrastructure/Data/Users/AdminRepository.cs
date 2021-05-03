276dd9a7e63dd95696884eb1b98140632c7c1364
Manuel Chinellato <manuelchine@libero.it>
2021-05-03 12:01:10 +0200

    primo push

---
+++ HollowMindsDev.BackEnd.Infrastructure/Data/Users/AdminRepository.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using HollowMindsDev.BackEnd.ApplicationCore.Entities.Users;
+using HollowMindsDev.BackEnd.ApplicationCore.Interfaces.IUsers;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HollowMindsDev.BackEnd.Infrastructure.Data.Users
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly string _connectionString;
+
+        public AdminRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Database");
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Admin> GetAll()
+        {
+            const string query = @"
+SELECT
+    mail as EMail
+FROM administrator;";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.Query<Admin>(query);
+        }
+
+        public Admin GetById(int id)
+        {
+            const string query = @"
+SELECT
+    mail as EMail
+FROM administrator
+WHERE idAdmin = @idA;";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryFirstOrDefault<Admin>(query);
+        }
+
+        public bool IfIsAdmin(string mail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Admin model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Admin model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
